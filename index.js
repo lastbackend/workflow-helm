d046532ab8c17e52280cbcfa46e52ab1b1a518ec
Alexander Kaloshin <alexander@lastbackend.com>
2023-06-18 17:04:29 +0300

    update import gpg key command block

--- index.js
+++ index.js
@@ -250,10 +250,8 @@ async function run() {
 
     // Import private key if provided
     if (!!secretKey) {
-      core.debug("save secret file to temporary file");
-      await exec.exec("echo", secretKey + " > secret.key.asc");
-      core.debug("import secret file to gpg keyring");
-      await exec.exec("gpg", "--batch --import secret.key.asc");
+      await exec.exec("echo", "'" + secretKey + "' > secret.key.asc");
+      await exec.exec("gpg", "--import secret.key.asc");
     }
 
     // Actually execute the deployment here.
